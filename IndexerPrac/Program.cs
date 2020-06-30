44dc82a1d59a72b20c495d8ccc3d421a439b1219
Gihun Ham <ham@e-mirim.hs.kr>
2020-06-30 13:50:52 +0900

    초기 커밋.

---
+++ IndexerPrac/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerPrac
+{
+    class Products
+    {
+        public int this[int i]
+        {
+            get { return i; }
+            set { Console.WriteLine(i); }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Products p = new Products();
+            Console.WriteLine(p[3]);
+        }
+    }
+}
